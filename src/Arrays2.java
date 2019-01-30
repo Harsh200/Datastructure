d067a9855ecfe89d363c373d3e497a771a2d5ba1
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-01-30 11:44:41 +0530

    making getter and setter methid

---
+++ src/Arrays2.java
@@ -0,0 +1,37 @@
+public class Arrays2 {
+    public static void main(String[] args) {
+        Student student =new Student();
+  /*      student.name="GUDDU";
+        student.rollno=123;
+        Student[] students=new Student[47];
+        for(int i=0;i<students.length;i++){
+            students[i]=new Student();
+            students[i].rollno=i+1;
+            students[i].name="GUDDU"+i;
+        }
+       for (Student student1: students){
+            System.out.println(student1);
+            }
+*/
+
+        }
+    }
+
+class Student{
+   private int rollno;
+private String name;
+ public int getRollno(){
+    return  rollno;
+}
+public String getName() {
+    return name;
+}
+public void setRollno(int rollno){
+
+}
+
+    @Override
+    public String toString() {
+        return (rollno +", " +name);
+    }
+}
