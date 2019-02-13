352ca234ac17a320d26652a4057aa45b4f0758a4
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-13 11:40:49 +0530

    declare array that initialise square of its index

---
+++ src/Arrayques1.java
@@ -0,0 +1,11 @@
+public class Arrayques1 {
+    public static void main(String[] args) {
+        int [] array=new int[10];
+        for(int i=0;i<10;i++){
+            array[i]=i*i;
+        }
+        for(int j=0;j<10;j++){
+            System.out.println(array[j]);
+        }
+    }
+}
--- src/Arrays2.java
+++ src/Arrays2.java
@@ -1,17 +1,34 @@
+/*
 public class Arrays2 {
-    public static void main(String[] args) {
+
+     private Student[] studentArray;
+    int bottom;
+
+
         // now we are implementing
-        Student[] studentArray;
-        int bottom;
+
         Arrays2(int numberOfStudent){
             studentArray=new Student[numberOfStudent];
             bottom=-1;
+
         }
+        private void insert(Student student){}
+        if (bottom ! studentArray.length-1){
+            bottom++:
+        studentArray[bottom]=student;
+            }
+    }
+
+    public static void main(String[] args) {
         Student student =new Student();
         student.setRollno(1);
         student.setName("Bablu");
         System.out.println(student);
-  /*      student.name="GUDDU";
+        Arrays2 list= new Arrays2(47);
+
+        list.insert(new Student()
+        )
+      student.name="GUDDU";
         student.rollno=123;
         Student[] students=new Student[47];
         for(int i=0;i<students.length;i++){
@@ -22,7 +39,8 @@ public class Arrays2 {
        for (Student student1: students){
             System.out.println(student1);
             }
-*/
+
+
 
         }
     }
@@ -48,3 +66,4 @@ public void setName(String name){
         return (rollno +", " +name);
     }
 }
+*/
